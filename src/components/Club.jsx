db247543d7f7852e2dd4726ec1cf10add3575e84
haram <dlfnadl97@naver.com>
2025-12-07 21:37:08 +0900

    Fix: Club.jsx 에 필터 추가

--- src/components/Club.jsx
+++ src/components/Club.jsx
@@ -144,10 +144,22 @@ const Club = () => {
                   >
                     <option value="all">전체</option>
                     <option value="서울특별시">서울특별시</option>
-                    <option value="부산광역시">부산광역시</option>
-                    <option value="인천광역시">인천광역시</option>
-                    <option value="대구광역시">대구광역시</option>
                     <option value="경기도">경기도</option>
+                    <option value="경상남도">경상남도</option>
+                    <option value="경상북도">경상북도</option>
+                    <option value="전라남도">전라남도</option>
+                    <option value="전라북도">전라북도</option>
+                    <option value="충청남도">충청남도</option>
+                    <option value="충청북도">충청북도</option>
+                    <option value="강원특별자치도">강원특별자치도</option>
+                    <option value="대구광역시">대구광역시</option>
+                    <option value="대전광역시">대전광역시</option>
+                    <option value="부산광역시">부산광역시</option>
+                    <option value="울산광역시">울산광역시</option>
+                    <option value="인천광역시">인천광역시</option>
+                    <option value="세종특별자치시">세종특별자치시</option>
+                    <option value="광주광역시">광주광역시</option>
+                    <option value="제주특별자치도">제주특별자치도</option>
                   </StSelect>
                   <StSelectIcon>
                     <ChevronDown size={16} />
@@ -166,10 +178,15 @@ const Club = () => {
                   >
                     <option value="all">전체</option>
                     <option value="농구">농구</option>
+                    <option value="스키">스키</option>
                     <option value="축구">축구</option>
                     <option value="배드민턴">배드민턴</option>
                     <option value="탁구">탁구</option>
                     <option value="게이트볼">게이트볼</option>
+                    <option value="육상">육상</option>
+                    <option value="수영">수영</option>
+                    <option value="볼링">볼링</option>
+                    <option value="골프">골프</option>
                   </StSelect>
                   <StSelectIcon>
                     <ChevronDown size={16} />
